4bf43509bf36acd3f4779038e12d4024eb9caccb
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-09-11 18:01:35 -0500

    Add logic to transaction controller

--- src/main/java/com/emazon/ms_transaction/infra/input/rest/TransactionController.java
+++ src/main/java/com/emazon/ms_transaction/infra/input/rest/TransactionController.java
@@ -1,17 +1,26 @@
 package com.emazon.ms_transaction.infra.input.rest;
 
+import com.emazon.ms_transaction.application.dto.SupplyReqDTO;
+import com.emazon.ms_transaction.application.handler.ITransactionHandler;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/transactions")
+@RequiredArgsConstructor
 public class TransactionController {
 
+    private final ITransactionHandler transactionHandler;
+
     @PostMapping("/supply")
-    public ResponseEntity<String> addSupply() {
-        return ResponseEntity.ok("Supply registered");
+    public ResponseEntity<String> addSupply(@RequestBody @Valid SupplyReqDTO dto) {
+        return ResponseEntity.status(HttpStatus.OK).body(transactionHandler.addSupply(dto));
     }
 
     @PostMapping("/sale")

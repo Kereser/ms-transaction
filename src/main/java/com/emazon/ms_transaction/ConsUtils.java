dd94317aaddce578611530722d8211ed1f560062
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-09-12 21:26:57 -0500

    Bean config and update constants

--- src/main/java/com/emazon/ms_transaction/ConsUtils.java
+++ src/main/java/com/emazon/ms_transaction/ConsUtils.java
@@ -4,6 +4,8 @@ public class ConsUtils {
     private ConsUtils() {
     }
 
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER = "Bearer ";
     /*** DB ***/
     public static final boolean FALSE = false;
 }

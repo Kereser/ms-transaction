bd6f40b3dbd7975acab8145ed958260db305fb54
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-09-12 21:31:30 -0500

    Req DTO

---
+++ src/main/java/com/emazon/ms_transaction/application/dto/supply/SupplyReqDTO.java
@@ -0,0 +1,39 @@
+package com.emazon.ms_transaction.application.dto.supply;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SupplyReqDTO {
+
+    @NotNull
+    @Positive
+    private Long userId;
+
+    @NotNull
+    @Valid
+    private Set<ItemQuantity> item;
+
+    @Builder
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ItemQuantity {
+        @NotNull
+        @Positive
+        private Long articleId;
+
+        @NotNull
+        @Positive
+        private Long quantity;
+    }
+}

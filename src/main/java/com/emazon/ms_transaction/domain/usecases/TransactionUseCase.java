c9575d482b26b5ba290cb3f8f1ca5d25eb61b55c
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-09-29 21:20:22 -0500

    New flow for register sale

--- src/main/java/com/emazon/ms_transaction/domain/usecases/TransactionUseCase.java
+++ src/main/java/com/emazon/ms_transaction/domain/usecases/TransactionUseCase.java
@@ -2,6 +2,8 @@ package com.emazon.ms_transaction.domain.usecases;
 
 import com.emazon.ms_transaction.application.mapper.TransactionDTOMapper;
 import com.emazon.ms_transaction.domain.api.ITransactionServicePort;
+import com.emazon.ms_transaction.domain.model.Sale;
+import com.emazon.ms_transaction.domain.model.SaleStatus;
 import com.emazon.ms_transaction.domain.model.Supply;
 import com.emazon.ms_transaction.domain.spi.ITransactionPersistencePort;
 import com.emazon.ms_transaction.domain.spi.StockFeignPort;
@@ -25,6 +27,48 @@ public class TransactionUseCase implements ITransactionServicePort {
         stockFeignPort.addSupply(transactionDTOMapper.toSetDTO(supply.getSupplyArticle()));
 
         save(supply);
+    }
+
+    @Override
+    public void registerSale(Sale sale) {
+        saveInProgressSale(sale);
+        processPayment(sale);
+        saveCompletedSale(sale);
+    }
+
+    private void saveCompletedSale(Sale sale) {
+        makeSaleCompleted(sale);
+        save(sale);
+    }
+
+    private void saveInProgressSale(Sale sale) {
+        makeSaleInProgress(sale);
+        save(sale);
+    }
+
+    private void makeSaleInProgress(Sale sale) {
+        sale.setStatus(SaleStatus.IN_PROGRESS);
+    }
+
+    private void makeSaleCompleted(Sale sale) {
+        sale.setStatus(SaleStatus.COMPLETED);
+    }
+
+    private void processPayment(Sale sale) {
+        assert sale.getSaleArticles() != null;
+        /*
+        * try {
+        *   ms-xxx to get payment data?
+        * } catch (Exception e) {
+        *   updateSaleToFailed();
+        * }
+        * */
+    }
+
+    private void save(Sale sale) {
+        sale.setTransactionDate(LocalDateTime.now());
+
+        transactionPersistencePort.save(sale);
     }
 
     private void save(Supply supply) {
